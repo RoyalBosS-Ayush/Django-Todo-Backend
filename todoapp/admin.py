137ec7f793fd9aebb99b5523e60987735a0a15aa
RoyalBosS <ayushmunot921@gmail.com>
2023-12-26 16:57:02 +0530

    todo admin panel configuration

--- todoapp/admin.py
+++ todoapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import TodoItem, Tag
 
-# Register your models here.
+
+@admin.register(TodoItem)
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = ("title", "timestamp", "status")
+    list_filter = ("status",)
+    search_fields = ("title", "description")
+    readonly_fields = ("timestamp",)
+    fieldsets = (
+        (
+            "Task Information",
+            {"fields": ("title", "description", "due_date", "tags", "status")},
+        ),
+    )
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
